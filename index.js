45ec3015c4a456454485348a9a2cb3c846c0fc0a
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-05-07 09:55:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// bootstrap es un framework para desarrollar sitios web adaptables
+// es una estructura es una base que puedes para desarrollar tu app o web de forma mas rapida y eficiente. 
+// adaptable 
+
+// 3 pilares de boostrap 
+// grid 
+//componentes es un carrosel
+// iconos
+
+// grid = cuadricula cojunto de contenedores, filas y columnas que definen como se va a presentar y a alinear el contenido 
+
+// clases para la grid 
+// . row = fila 
+// . col- = column - xs extra small <576px
+// .col-sm significa muy pequeno > 576px
+// col-md- medium = mediano >768px
+// col-lg- = large >992px
+// col-xl- = extra large  >1200px
+// xxl >1400px
+
+// viewport
+
+// breakpoint a partir de la cual podemos cambiar el estilo o la estrcutura de la pagina web
